c7623f4a3155c2205fd2461b88371ce3bb9232f7
nakeeta <abc@example.com>
2020-05-15 00:42:57 +0000

    linux_sql

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -1,40 +1,38 @@
 #!/bin/bash
-option= $1
-user_name= $2
-password= $3
+
+option=$1
+username=$2
+password=$3
 
 systemctl status docker || systemctl start docker
-# case create
-if [$option =="create" ]; then
+if [[ $option = "create" ]]; then
   if [[ "$user_name" == "0" ]] || [[ "$password" == "0" ]]; then
     echo "Error : user name or password not passed through CLI"
-    exit 1
+    exit $?
   fi
-  # if container exists print error message
-  if [$user_name =="jarvis-psql" ]; then
-    echo "Error: Container is already created"
-    exit 1
-      #otherwise create container
-      else
-        docker run --name jrvs-psql -e POSTGRES_PASSWORD=${password} -e POSTGRES_USER=${user_name} -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
-        exit $?
+
+  if [[ $(docker container ls -a -f name=jrvs-psql | wc -l) -ge 2 ]]; then
+        echo "Error: Container is already created"
+    else
+      docker run --name jrvs-psql -e POSTGRES_PASSWORD=$password -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 $username
+      exit $?
     fi
-
 fi
-# case start
-if [$option =="start" ];then
-  # check if container is already created, if so start container
-  if [[ $(docker container ls -a -f name=jrvs-psql |wc -l) -ge 2]]; then
-    docker container start jrvs-psql
-    exit$?
-  fi
-fi
-#case stop
-if [$option =="stop" ]; then
-    docker container stop jrvs-psql
-    exit$?
-  fi
 
-#case invalid
-echo "Error: invaid entry"
+if [[ $option = "start" ]]; then
+  if [[ $(docker container ls -a -f name=jrvs-psql | wc -l) -ge 2 ]]; then
+   docker container start jrvs-psql
+   echo "Successfully started"
+    else
+   echo "jrvs-psql container has not been created"
+ fi
+   exit $?
+fi
+
+if [[ $option = "stop" ]]; then
+   docker container stop jrvs-psql
+   echo "Successfully stopped"
+   exit $?
+fi
+echo "Error: Invaid Entry"
 exit 0

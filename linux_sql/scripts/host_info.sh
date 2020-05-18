7d7ee41606f3bce120b609f86332859ae90631d0
nakeeta <abc@example.com>
2020-05-18 20:47:26 +0000

    linux_sql

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -10,19 +10,19 @@ export PGPASSWORD=$psql_password
 # parse host hardware specifications using bash cmds
 lscpu='lscpu'
 hostname=$(hostname -f)
-cpu_number=$(echo "$lscpu"  | grep "^CPU\(s\):" | xargs)
-cpu_architecture=$(echo "$lscpu" | grep "Architecture:" | xargs)
-cpu_model=$(echo "$lscpu" | grep "Model\sname:"| xargs)
-cpu_mhz=$(echo "$lscpu" | grep "CPU\sMHz:"| xargs )
-l2_cache=$(echo "$lscpu" | grep "L2\scache:" | xargs)
-total_mem=$(cat /proc/meminfo | grep "MemTotal")
-timestamp=$(date +'%Y12%d %H:%M')
+cpu_number=$(echo "$lscpu"  | egrep "^CPU\(s\):" |awk '{print $2}'| xargs)
+cpu_architecture=$(echo "$lscpu" | grep "Architecture:" |awk '{print $2}'| xargs)
+cpu_model=$(echo "$lscpu" | grep "Model\sname:"|awk -F ": " '{print $2}'| xargs)
+cpu_mhz=$(echo "$lscpu" | grep  "CPU\sMHz:"|awk -F ": " '{print $2}'| xargs )
+l2_cache=$(echo "$lscpu" | grep "L2\scache:" | egrep -o "[0-9]{2,}"|xargs)
+total_mem=$(cat /proc/meminfo | grep "MemTotal"| egrep -o "[0-9]{2,}"|xargs)
+timestamp=$(date '+%Y-%m-%d %T')
 
 # insert staemaent
 in_state="INSERT INTO host_info (hostname,cpu_number, cpu_architecture, cpu_model, cpu_mhz,l2_cache,total_mem,timestamp)
 VALUES ('"$hostname"', '"$cpu_num"','"$cup_architecture"','"$cpu_model"','"$cpu_mhz"','"$l2_cache"', '"$total_mem"','"$timestamp"')"
 
 # connect to PSQL instance
-psql -h $psql_host -p $psql_port -U $db_name -d $psql_user -W $psql_password -c "$in_state"
+psql psql -h $psql_host -U $psql_user  -d $db_name -c "$in_state"
 
 exit $?
--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -8,21 +8,23 @@ psql_password=$5
 export PGPASSWORD=$psql_password
 
 # parse host hardware specifications using bash cmds
-specs='lscpu'
-vm= 'vmstat'
-disk= 'df'
-timestamp=$(date +'%Y12%d %H:%M')
-memory_free=$(cat /proc/meminfo | grep "MemFree"| xargs)
+lscpu=`lscpu`
+vm= `vmstat`
+disk= `df`
+timestamp=$(date '+%Y-%m-%d %T')
+memory_free=$(cat /proc/meminfo | grep "MemFree"| egrep -o "[0-9]{2,}"| xargs)
 cpu_idle=$(echo "$vm" -t | awk '{ print $15}' |tail -1 | xargs)
-cpu_kernel=$(echo "$lscpu"  | grep "^CPU\(s\):" | xargs)
+cpu_kernel=$(echo "$vm" -t | awk '{print $14}' | tail -1 | xargs)
 disk_io=$(echo "$vm" -d| awk '{print $10}'| tail -1 |xargs)
-disk_available=$(echo "$disk"-BM | awk '{print $4}'|tail -1| xargs)
+disk_available=$(echo "$disk" -BM | awk '{print $4}'|head -6| tail -1| egrep -o '[0-9]+'| xargs)
+
+
 
 # insert staemaent
 in_state=" INSERT INTO host_usage (timestamp,memory_free, cpu_idle, cpu_kernel,disk_io,disk_available)
 VALUES ('"$timestamp"', '"$memory_free"','"$cpu_idle"','"$cpu_kernel"','"$disk_io"','"$disk_available"')"
 
-# connect to PSQL instance
-psql -h $psql_host -p $psql_port -U $db_name -d $psql_user -W $psql_password -c "$in_state"
+# Connect to PSQL instance
+psql psql -h $psql_host -U $psql_user  -d $db_name -c "$in_state"
 
 exit $?

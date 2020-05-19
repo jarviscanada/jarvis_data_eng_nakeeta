2c4707fafb4bbace270425b8f77c17ed1cf93c93
Nakeetak <64488220+Nakeetak@users.noreply.github.com>
2020-05-18 20:56:26 -0400

    Update queries.sql

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -7,16 +7,11 @@ ORDER BY total_mem DESC;
 
 -- Question 2
 -- Average memeory usage
-SELECT host_info.id,host_info.hostname,
-        -- find Average used memory in percentage over 5 mins interval
-      avg_used_mem_percentage(
-        AVG(host_info.total_mem - host_usage.memory_free)*100/host_info.total_mem)
-          OVER(
-            PARTITION BY (
-              hour('hour', host_usage.timestamp)+ minute('min',host_info.timestamp) :: int 5
-            )
-          )
-      )
-FROM host_info, host_usage
-INNER JOIN host_usage on host_info.id=host_usage.id
-ORDER by host_usage.id 
+SELECT 
+	id, hostname,
+	to_char( to_timestamp(round(extract(epoch from host_usage.timestamp) / 300) * 300), 'YYYY-MM-DD HH24:MI:SS') AS truncated_timestamp,
+	(AVG(total_mem-memory_free)*100/total_mem)::INTEGER AS avg_used_mem_percentage 
+FROM host_info 
+INNER JOIN  host_usage ON host_info.id=host_usage.host_id 
+GROUP BY truncated_timestamp, id 
+ORDER BY id ASC;

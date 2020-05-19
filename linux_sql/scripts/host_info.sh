867d0a13b4df4be663dcad2857fc02d872099ac0
Nakeetak <64488220+Nakeetak@users.noreply.github.com>
2020-05-18 22:00:46 -0400

    Update host_info.sh

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# assign CLI arguments to variables
+##assign CLI arguments to variables
 psql_host=$1
 psql_port=$2
 db_name=$3
@@ -8,21 +8,20 @@ psql_password=$5
 export PGPASSWORD=$psql_password
 
 # parse host hardware specifications using bash cmds
-lscpu='lscpu'
+cpu_list=`lscpu`
 hostname=$(hostname -f)
-cpu_number=$(echo "$lscpu"  | grep "^CPU\(s\):" | xargs)
-cpu_architecture=$(echo "$lscpu" | grep "Architecture:" | xargs)
-cpu_model=$(echo "$lscpu" | grep "Model\sname:"| xargs)
-cpu_mhz=$(echo "$lscpu" | grep "CPU\sMHz:"| xargs )
-l2_cache=$(echo "$lscpu" | grep "L2\scache:" | xargs)
-total_mem=$(cat /proc/meminfo | grep "MemTotal")
-timestamp=$(date +'%Y12%d %H:%M')
+cpu_number=$(echo "$cpu_list"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$cpu_list" | grep "Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$cpu_list" | grep "Model\sname:"| awk -F ": " '{print $2}' | xargs)
+cpu_mhz=$(echo "$cpu_list" | grep  "CPU\sMHz:"| awk -F ": " '{print $2}' | xargs )
+l2_cache=$(echo "$cpu_list" | grep "L2\scache:" | egrep -o "[0-9]{2,}" |xargs)
+total_mem=$(cat /proc/meminfo | grep "MemTotal"| egrep -o "[0-9]{2,}" |xargs)
+timestamp=$(date '+%Y-%m-%d %T')
 
-# insert staemaent
-in_state="INSERT INTO host_info (hostname,cpu_number, cpu_architecture, cpu_model, cpu_mhz,l2_cache,total_mem,timestamp)
-VALUES ('"$hostname"', '"$cpu_num"','"$cup_architecture"','"$cpu_model"','"$cpu_mhz"','"$l2_cache"', '"$total_mem"','"$timestamp"')"
+##insert staemaent
+stat="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz,L2_cache, total_mem, timestamp) VALUES ('$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz','$l2_cache', '$total_mem', '$timestamp');"
 
-# connect to PSQL instance
-psql -h $psql_host -p $psql_port -U $db_name -d $psql_user -W $psql_password -c "$in_state"
+##executing insert command
+psql -h $psql_host -U $psql_user  -d $db_name -c "$stat"
 
-exit $?
+exit 0
